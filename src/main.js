0df18c146f1c7d26f323a196273a64e313e1e723
Kajaia <lashakajaia31@gmail.com>
2022-07-20 16:26:26 +0400

    Installed sweetalert, bootstrap and fontawesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import "sweetalert2/dist/sweetalert2.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "@fortawesome/fontawesome-free/js/all.min.js";
+
 const app = createApp(App);
 
 app.use(createPinia());
