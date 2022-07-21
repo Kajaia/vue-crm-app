294f0dd39832559b0e5b6fa2c77dd442af81a41d
Kajaia <lashakajaia31@gmail.com>
2022-07-21 14:41:38 +0400

    Added localStorage for cards

--- src/stores/cards.js
+++ src/stores/cards.js
@@ -1,46 +1,52 @@
 import { defineStore } from "pinia";
 
+const updateCardsStorage = (cards) => {
+  localStorage.setItem("cards", JSON.stringify(cards));
+};
+
 export const useCardsStore = defineStore({
   id: "cards",
   state: () => ({
-    cards: [
-      {
-        id: 1,
-        title: "ToDo",
-        position: 1,
-        board_id: 2,
-      },
-      {
-        id: 2,
-        title: "In Progress",
-        position: 2,
-        board_id: 2,
-      },
-      {
-        id: 3,
-        title: "Completed",
-        position: 3,
-        board_id: 2,
-      },
-      {
-        id: 4,
-        title: "ToDo",
-        position: 1,
-        board_id: 1,
-      },
-      {
-        id: 5,
-        title: "In Progress",
-        position: 1,
-        board_id: 3,
-      },
-      {
-        id: 6,
-        title: "Completed",
-        position: 2,
-        board_id: 3,
-      },
-    ],
+    cards: localStorage.cards
+      ? JSON.parse(localStorage.cards)
+      : [
+          {
+            id: 1,
+            title: "ToDo",
+            position: 1,
+            board_id: 2,
+          },
+          {
+            id: 2,
+            title: "In Progress",
+            position: 2,
+            board_id: 2,
+          },
+          {
+            id: 3,
+            title: "Completed",
+            position: 3,
+            board_id: 2,
+          },
+          {
+            id: 4,
+            title: "ToDo",
+            position: 1,
+            board_id: 1,
+          },
+          {
+            id: 5,
+            title: "In Progress",
+            position: 1,
+            board_id: 3,
+          },
+          {
+            id: 6,
+            title: "Completed",
+            position: 2,
+            board_id: 3,
+          },
+        ],
   }),
   actions: {
     getCardsByBoardId(id) {
@@ -53,12 +59,18 @@ export const useCardsStore = defineStore({
         position: this.cards.length + 1,
         board_id: boardId,
       });
+
+      updateCardsStorage(this.cards);
     },
     removeCard(id) {
       this.cards = this.cards.filter((card) => card.id !== id);
+
+      updateCardsStorage(this.cards);
     },
     clearCardsByBoardId(id) {
       this.cards = this.cards.filter((card) => card.board_id !== id);
+
+      updateCardsStorage(this.cards);
     },
   },
 });
