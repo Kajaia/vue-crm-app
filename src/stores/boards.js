88396020815bd8189ec7ec160870e63f16196996
Kajaia <lashakajaia31@gmail.com>
2022-07-21 14:36:48 +0400

    Added localStorage for boards

--- src/stores/boards.js
+++ src/stores/boards.js
@@ -1,34 +1,40 @@
 import { defineStore } from "pinia";
 
+const updateBoardsStorage = (boards) => {
+  localStorage.setItem("boards", JSON.stringify(boards));
+};
+
 export const useBoardsStore = defineStore({
   id: "boards",
   state: () => ({
-    boards: [
-      {
-        id: 1,
-        title: "Lorem",
-        description: "Dolor, sit amet...",
-        color: "bg-warning",
-      },
-      {
-        id: 2,
-        title: "Ipsum",
-        description: "Dolor, sit amet...",
-        color: "bg-danger",
-      },
-      {
-        id: 3,
-        title: "Amet",
-        description: "Dolor, sit amet...",
-        color: "bg-success",
-      },
-      {
-        id: 4,
-        title: "Dolor",
-        description: "Dolor, sit amet...",
-        color: "bg-info",
-      },
-    ],
+    boards: localStorage.boards
+      ? JSON.parse(localStorage.boards)
+      : [
+          {
+            id: 1,
+            title: "Lorem",
+            description: "Dolor, sit amet...",
+            color: "bg-warning",
+          },
+          {
+            id: 2,
+            title: "Ipsum",
+            description: "Dolor, sit amet...",
+            color: "bg-danger",
+          },
+          {
+            id: 3,
+            title: "Amet",
+            description: "Dolor, sit amet...",
+            color: "bg-success",
+          },
+          {
+            id: 4,
+            title: "Dolor",
+            description: "Dolor, sit amet...",
+            color: "bg-info",
+          },
+        ],
   }),
   actions: {
     getBoard(id) {
@@ -41,9 +47,13 @@ export const useBoardsStore = defineStore({
         description: description,
         color: color,
       });
+
+      updateBoardsStorage(this.boards);
     },
     removeBoard(id) {
       this.boards = this.boards.filter((board) => board.id !== id);
+
+      updateBoardsStorage(this.boards);
     },
   },
 });
